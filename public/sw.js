d7cb5468307f66a114d3d07e39bd33baba53431c
mengbohui <18146512269@163.com>
2026-07-30 14:45:27 +0800

    fix: refresh PWA cache after release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'guangyao-skin-v3'
+const CACHE_NAME = 'guangyao-skin-v4'
 const BASE_PATH = new URL(self.registration.scope).pathname
 const fromBase = (path = '') => `${BASE_PATH}${path}`
 const APP_SHELL = [
